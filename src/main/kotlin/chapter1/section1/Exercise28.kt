4f41e8f3b1b29d0c56b732ff4e32778b072df440
rupeshsasne <rupeshsasne@gmail.com>
2021-04-25 16:47:54 +0530

    solve exercise 28 - remove duplicates from whitelist after sort.

---
+++ src/main/kotlin/chapter1/section1/Exercise28.kt
@@ -0,0 +1,27 @@
+package chapter1.section1
+
+import edu.princeton.cs.algs4.In
+import edu.princeton.cs.algs4.StdIn
+import edu.princeton.cs.algs4.StdOut
+import java.lang.IllegalArgumentException
+
+fun main(args: Array<String>) {
+    var whitelist = In(args[0]).readAllInts()
+    whitelist.sort()
+    whitelist = whitelist.distinct().toIntArray()
+
+    val operator: Char = args[1][0]
+
+    if (operator != '+' && operator != '-')
+        throw IllegalArgumentException("Invalid argument $operator")
+
+    while (!StdIn.isEmpty()) {
+        val key = StdIn.readInt()
+        val result = BinarySearch.rank(key, whitelist)
+
+        when {
+            operator == '+' && result == -1 -> StdOut.println(key)
+            operator == '-' && result != -1 -> StdOut.println(key)
+        }
+    }
+}
